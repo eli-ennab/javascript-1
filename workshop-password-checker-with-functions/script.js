09a6bfa1094298759e4ac0f79ee0b6cd1501168b
Eli Ennab <elinaennab@gmail.com>
2022-11-09 10:20:20 +0100

    Adding previous code that this will be based on.

--- workshop-password-checker-with-functions/script.js
+++ workshop-password-checker-with-functions/script.js
@@ -27,10 +27,6 @@
  * - eller har minst 16 tecken
  */
 
- const specialChars = [
-	"@", "$", "%", "*", "^", "<", ">", "?", "!", "(", ")", "[", "]", "{", "}", "'"
-];
-
 let password;
 // password = "password"; // inte giltigt
 // password = "p@$swo"; // giltigt
@@ -40,3 +36,38 @@ password = "p@ssw%rd"; // giltigt
 // password = "secretpassword"; // inte giltigt
 // password = "secret-password"; // giltigt
 // password = "such-password-much-secure-very-long"; // giltigt
+
+password.length;
+const specialChars = [
+    "@", "$", "%", "*", "^", "<", ">", "?", "!", "(", ")", "[", "]", "{", "}", "'"
+];
+
+// set status variable to initial value false
+let specialCharCount = 0 // 0 har Boolean-värde false!
+
+// iterate (loop) over each item in specialChars
+for (let i = 0; i < specialChars.length; i++) {
+    const specialChar = specialChars[i];
+
+    if (password.includes(specialChar)) {
+		specialCharCount++
+    }
+}
+
+console.log(`Checking password... "${password}"`);
+
+if (password.length >= 16) {
+    console.log(`Congrats, your password is at least 16 characters long!`);
+
+} else if (password.length >= 12 && password.includes('-')){
+    console.log(`Congrats, your password is at least 12 characters long and contains a hyphen.`);
+
+} else if (password.length >= 8 && specialCharCount >= 1){
+    console.log(`Congrats, your password is at least 8 characters long and contains at least one special char.`);
+
+} else if (password.length >= 6 && specialCharCount >= 2) {
+    console.log(`Congrats, your password is at least 6 characters long and contains at least two different special chars.`);
+
+} else {
+    console.log(`Insecure password. Try again!`);
+}
