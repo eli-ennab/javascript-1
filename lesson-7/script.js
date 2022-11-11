36ca79339b2da3d795f66c0e76e356900f6f87d9
Eli Ennab <elinaennab@gmail.com>
2022-11-11 11:29:41 +0100

    Form with event listener.

--- lesson-7/script.js
+++ lesson-7/script.js
@@ -1,3 +1,17 @@
 /**
-*
- */
+ * Form and form events
+*/
+
+const contactForm = document.querySelector('#contact');
+
+contactForm.addEventListener('submit', e => {
+		// prevent form from being submitted
+		e.preventDefault();
+
+		console.log('Form submitted but default behaviour prevented.');
+
+        console.log("First name:", contactForm.first_name.value);
+        console.log("Last name:", contactForm.last_name.value);
+});
+
+// contactForm.first_name.value
