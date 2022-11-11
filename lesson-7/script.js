9da673458ccb78c14bdd2859fb0a7d46c98a08b2
Eli Ennab <elinaennab@gmail.com>
2022-11-11 11:56:40 +0100

    Form with a passwordchecker from earlier workshop.

--- lesson-7/script.js
+++ lesson-7/script.js
@@ -2,16 +2,54 @@
  * Form and form events
 */
 
-const contactForm = document.querySelector('#contact');
+const passwordForm = document.querySelector('#passwordForm');
 
-contactForm.addEventListener('submit', e => {
+passwordForm.addEventListener('submit', e => {
 		// prevent form from being submitted
 		e.preventDefault();
 
 		console.log('Form submitted but default behaviour prevented.');
 
-        console.log("First name:", contactForm.first_name.value);
-        console.log("Last name:", contactForm.last_name.value);
+        // Get the password that the user submitted
+        const password = passwordForm.password.value;
+        // console.log("Password:", passwordForm.password.value);
+
+        console.log(`Checking password '${password}'`);
+ 
+        if (password.length >= 16) {
+            alert("Great! That's a long password.");
+        
+        } else if (password.length >= 12 && password.includes('-')) {
+           alert("Great! That's a pretty good password.");
+        
+        } else if (password.length >= 8 && countSpecialChars(password)) {
+           alert("Great! Such password, much secure, very hard to crack.");
+        
+        } else if (password.length >= 6 && countSpecialChars(password) >= 2) {
+           alert("Great! Such password, much secure, VERY hard to crack.");
+        
+        } else {
+           alert("Insecure password.");
+        }
 });
 
-// contactForm.first_name.value
+const specialChars = [
+    "@", "$", "%", "*", "^", "<", ">", "?", "!", "(", ")", "[", "]", "{", "}", "'"
+];
+
+const countSpecialChars = (pwd) => {
+    let specialCharCount = 0
+ 
+        for (let i = 0;
+            i < specialChars.length;
+            i++
+        ) {
+            const specialChar = specialChars[i]
+        
+            if (pwd.includes(specialChar)) {
+                specialCharCount++
+            }
+        }
+
+        return specialCharCount;
+};
