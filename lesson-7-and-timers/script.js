4f3321a9e0f7a78bb7dc55b3d9a4ae794c644881
Eli Ennab <elinaennab@gmail.com>
2022-11-11 13:56:31 +0100

    Scary timer-event with ghost.

--- lesson-7-and-timers/script.js
+++ lesson-7-and-timers/script.js
@@ -5,3 +5,21 @@
 
  const btnScareMe = document.querySelector('#btnScareMe');
  const ghostEl = document.querySelector('#ghost');
+
+ btnScareMe.addEventListener('click', () => {
+    // hide button
+    btnScareMe.classList.add('hide');
+
+    // start timer
+    setTimeout ( () => {
+        // this will be executed after 2000 milliseconds (2 seconds)
+        // alert('Boo!');
+        ghostEl.classList.remove('hide');
+
+        //
+        setTimeout (() => {
+        ghostEl.classList.add('hide');
+            btnScareMe.classList.remove('hide');
+        }, 4000);
+    }, 2000 );
+});
