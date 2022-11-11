e376976ba1d04ccc18be7fecf903be5f37f57573
Eli Ennab <elinaennab@gmail.com>
2022-11-11 14:26:11 +0100

    Update with teachers code.

--- lesson-7-and-timers/script.js
+++ lesson-7-and-timers/script.js
@@ -5,23 +5,33 @@
 
  const btnScareMe = document.querySelector('#btnScareMe');
  const ghostEl = document.querySelector('#ghost');
-
+ 
+ const getRandomNumber = (max = 10) => {
+     return Math.ceil( Math.random() * max );
+ }
+ 
  btnScareMe.addEventListener('click', () => {
-    // hide button
-    btnScareMe.classList.add('hide');
-
-    const delay = getRandomNumber() * 1000;
-
-    // start timer
-    setTimeout ( () => {
-        // this will be executed after 2000 milliseconds (2 seconds)
-        // alert('Boo!');
-        ghostEl.classList.remove('hide');
-
-        //
-        setTimeout (() => {
-        ghostEl.classList.add('hide');
-            btnScareMe.classList.remove('hide');
-        }, 4000);
-    }, 2000 );
-});
+     // hide button
+     btnScareMe.classList.add('hide');
+ 
+     // get a random delay to wait before scaring user
+     const delay = getRandomNumber() * 1000;
+ 
+     console.log(`Starting timer for ${delay} ms before scaring user`);
+ 
+     // start timer
+     setTimeout( () => {
+         // this will be executed after 2000 milliseconds (2 seconds)
+         console.log("Scaring user...");
+         ghostEl.classList.remove('hide');  // show ghost
+ 
+         console.log("Starting timer for showing button again");
+ 
+         // timers for everyone!
+         setTimeout(() => {
+             console.log("Showing button again...");
+             ghostEl.classList.add('hide');  // hide ghost again
+             btnScareMe.classList.remove('hide');  // show button again
+         }, 4000);
+     }, delay );
+ });
