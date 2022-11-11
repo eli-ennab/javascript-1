519c066ac77fc2b8801a59843da251660c9b59ce
Eli Ennab <elinaennab@gmail.com>
2022-11-11 15:16:36 +0100

    A simple clock and better clock.

--- lesson-7-and-dates/script.js
+++ lesson-7-and-dates/script.js
@@ -1,4 +1,44 @@
 /**
- * Clock 🕰️
+ * Simple clock
  *
  */
+
+// const clockEl = document.querySelector('#clock');
+
+// // we could stop the time by calling `clearInterval(clockId)`
+// const clockId = setInterval( () => {
+//     // get current date and time
+//     const now = new Date();
+
+//     // output current time to `#clock`-element
+//     clockEl.innerText = now.toLocaleTimeString();
+
+//     // log current time to console
+//     // console.log("Time is:", now.toLocaleTimeString());
+// }, 1000);
+
+/**
+ * Better clock
+ *
+ */
+
+// get reference to element with id `clock`
+const clockEl = document.querySelector('#clock');
+
+const tick = () => {
+	// get current date and time
+	const now = new Date();
+
+	// output current time to `#clock`-element
+	clockEl.innerText = now.toLocaleTimeString();
+
+	if (now.getDay() === 5 && now.getHours() >= 15) {
+		clockEl.innerText += ' 🥳';
+	}
+}
+
+// we could stop the time by calling `clearInterval(clockId)`
+// start interval-timer with 1000 ms interval
+const clockId = setInterval(tick, 1000);
+
+tick();
