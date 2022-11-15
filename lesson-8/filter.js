b28b07f81b4ee30b1251c306b7d463e87ca738c9
Eli Ennab <elinaennab@gmail.com>
2022-11-15 13:58:38 +0100

    Filter any numbers >= 25 into a new array.

---
+++ lesson-8/filter.js
@@ -0,0 +1,20 @@
+/**
+ * Higher Order Array Methods
+ * * filter()
+*/
+
+// numbers
+const numbers = [ 47, 5, 21, 50, 24, 18, 47, 13, 28, 8, 18, 15, 11, 43, 7, 33, 52 ];
+
+console.log("Original numbers:", numbers);
+
+// filter and push out all numbers equal to or bigger than 25 (to a new array)
+const largeNumbers = numbers.filter( num => {
+    if (num >= 25) {
+        return true;
+    } else {
+        return false;
+    }
+} );
+
+console.log("Numbers >= 25:", largeNumbers);
