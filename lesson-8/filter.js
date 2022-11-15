cbb558e6bad610da824ac62552f40b36d837056c
Eli Ennab <elinaennab@gmail.com>
2022-11-15 14:06:09 +0100

    Filter longish names to a new array with one-liner.

--- lesson-8/filter.js
+++ lesson-8/filter.js
@@ -3,16 +3,22 @@
  * * filter()
 */
 
-// numbers
-const numbers = [ 47, 5, 21, 50, 24, 18, 47, 13, 28, 8, 18, 15, 11, 43, 7, 33, 52 ];
+// // numbers
+// const numbers = [ 47, 5, 21, 50, 24, 18, 47, 13, 28, 8, 18, 15, 11, 43, 7, 33, 52 ];
 
-console.log("Original numbers:", numbers);
+// console.log("Original numbers:", numbers);
 
-// // filter and push out all numbers equal to or bigger than 25 (to a new array)
-// const largeNumbers = numbers.filter( num => {
-//     return (num >= 25);
-// } );
+// // // filter and push out all numbers equal to or bigger than 25 (to a new array)
+// // const largeNumbers = numbers.filter( num => {
+// //     return (num >= 25);
+// // } );
 
-const largeNumbers = numbers.filter(num => num >= 25);
+// const largeNumbers = numbers.filter(num => num >= 25);
 
-console.log("Numbers >= 25:", largeNumbers);
+// console.log("Numbers >= 25:", largeNumbers);
+
+// names
+// filter out longish names to new array
+const names = ["Johan", "Kajsa", "Bo", "Li"];
+const longishNames = names.filter(name => name.length > 2);
+console.log("Longish names:", longishNames);
