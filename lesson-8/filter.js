c60454b721c1fd7e64aaeb1edc935847508ccf18
Eli Ennab <elinaennab@gmail.com>
2022-11-15 14:03:46 +0100

    One-liner filter any numbers >= 25.

--- lesson-8/filter.js
+++ lesson-8/filter.js
@@ -8,9 +8,11 @@ const numbers = [ 47, 5, 21, 50, 24, 18, 47, 13, 28, 8, 18, 15, 11, 43, 7, 33, 5
 
 console.log("Original numbers:", numbers);
 
-// filter and push out all numbers equal to or bigger than 25 (to a new array)
-const largeNumbers = numbers.filter( num => {
-    return (num >= 25);
-} );
+// // filter and push out all numbers equal to or bigger than 25 (to a new array)
+// const largeNumbers = numbers.filter( num => {
+//     return (num >= 25);
+// } );
+
+const largeNumbers = numbers.filter(num => num >= 25);
 
 console.log("Numbers >= 25:", largeNumbers);
