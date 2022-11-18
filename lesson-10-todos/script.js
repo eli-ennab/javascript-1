8b14e2a37b2588f95365d5f7853236c911c37b67
Eli Ennab <elinaennab@gmail.com>
2022-11-18 14:55:03 +0100

    Find max todo id and add 1 when new todo.

--- lesson-10-todos/script.js
+++ lesson-10-todos/script.js
@@ -4,21 +4,24 @@
  * HTML data-* Attribute 
 */
 
-// Get references to DOM elements
+// get references to DOM elements
 const todosEl = document.querySelector('#todos');
 const newTodoFormEl = document.querySelector('#new-todo-form');
 
-// List of todos
+// list of todos
 const todos = [
 	{
+		id: 1,
 		title: "Learn basic JavaScript",
 		completed: true,
 	},
 	{
-		title: "Learn DOM",
+		id: 2,
+		title: "Learn Array Methods",
 		completed: false,
 	},
 	{
+		id: 3,
 		title: "Take over the world",
 		completed: false,
 	},
@@ -26,50 +29,93 @@ const todos = [
 
 // Render todos to DOM
 const renderTodos = () => {
-    todosEl.innerHTML = '';
-    todos.forEach(todo => {
-        if (todo.completed) {
-            todosEl.innerHTML += `<li class="list-group-item completed">${todo.title}</li>`;
-        } else {
-        todosEl.innerHTML += `<li class="list-group-item">${todo.title}</li>`;
-        }
-    });
-};
+
+	todosEl.innerHTML = '';
+	todos.forEach(todo => {
+		let cssClasses = "list-group-item";
+
+		if (todo.completed) {
+			cssClasses += " completed";   // "list-group-item completed"
+		}
+
+		todosEl.innerHTML += `
+			<li class="${cssClasses}" data-todo-id="${todo.id}">
+				${todo.title}
+			</li>
+		`;
+
+	});
+}
 renderTodos();
 
-// Listen for click-events on the `#todos` (the `<ul>`)
+// Listen for click-events on `#todos` (the `<ul>`)
 todosEl.addEventListener('click', (e) => {
-    // Check if user clickes on a LI element
-    if (e.target.tagName === "LI") {
-        const clickedTodoTitle = e.target.innerText;
-        // Search todos for the todo with the title clickedTodoTitle
-        const clickedTodo = todos.find( (todo) => {
-            return todo.title === clickedTodoTitle;
-        } );
+	// console.log("You clicked on either the whole list, or on a listitem", e.target);
 
-        // Change completed status of found todo
-        clickedTodo.completed = !clickedTodo.completed;
+	// check if user clicked on a LI element
+	if (e.target.tagName === "LI") {
 
-        // Render updated todos
-        renderTodos();
-    }
+		// get the `data-todo-id` attribute from the LI element
+		const clickedTodoId = e.target.dataset.todoId;     // `data-todo-id`
+		console.log("You clicked on the listitem for todo with id:", clickedTodoId);
+
+		// search todos for the todo with the id todoId
+		const clickedTodo = todos.find( (todo) => {
+			return todo.id == clickedTodoId;
+		} );
+
+		// change completed-status of found todo
+		clickedTodo.completed = !clickedTodo.completed;
+
+		// render updated todos
+		renderTodos();
+	}
 });
 
 // Create a new todo when form is submitted
 newTodoFormEl.addEventListener('submit', (e) => {
-    e.preventDefault();
+	// Prevent form from being submitted (to the server)
+	e.preventDefault();
+
+    // extract all todo-ids
+    const maxTodoId = todos.reduce((maxId, todo) => {
+            return Math.max(todo.id, maxId);
+
+            // return (todo.id > maxId) 
+            // ? todo.id
+            // : maxId;
+
+            // if (todo.id > maxId) {
+            //     return todo.id;
+            // } 
+            
+            // return maxId;
+    }, 0);
+    const newTodoId = maxTodoId + 1;
 
     // Create and push new todo into array
-    todos.push({
-        title: newTodoFormEl.newTodo.value,
-        completed: false
-    });
+	todos.push({
+        id: newTodoId,
+		title: newTodoFormEl.newTodo.value,
+		completed: false,
+	});
+	console.log("created new todo...");
 
-    // Render new todo to DOM
-    renderTodos();
+	// Render new todo to DOM
+	renderTodos();
 
-    // Reset form
-    newTodoFormEl.reset();
+	// Empty input field
+	// newTodoFormEl.newTodo.value = '';
+
+	// Reset form
+	newTodoFormEl.reset();
 });
 
-
+/*
+// STOP USER FROM RESETTING FORM 😈
+newTodoFormEl.addEventListener('reset', e => {
+	// YOU NO RESET FORM, FORM RESETS YOU!
+	e.preventDefault();
+	alert("YOU NO RESET FORM, FORM RESETS YOU!");
+});
+*/
