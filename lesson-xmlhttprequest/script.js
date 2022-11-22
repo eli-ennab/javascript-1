dc28c2326240dd3d8360892a52529fac80e41e3b
Eli Ennab <elinaennab@gmail.com>
2022-11-22 14:38:42 +0100

    Our first XML Http Request.

--- lesson-xmlhttprequest/script.js
+++ lesson-xmlhttprequest/script.js
@@ -2,3 +2,25 @@
  * XMLHttpRequest
  *
  */
+
+// console.log(1);
+// console.log(2);
+
+// setTimeout(() => {
+//     console.log(3);
+// }, 2000);
+
+// console.log(4);
+// console.log(5);
+
+// Create a new XML Http Request
+const request = new XMLHttpRequest();
+
+// Set request to GET data from 'https://jsonplaceholder.typicode.com/users'
+request.open('GET', 'https://jsonplaceholder.typicode.com/users');
+
+// Send the request
+request.send();
+
+// Done?
+console.log('Request sent!');
