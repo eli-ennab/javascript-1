c5c635c97ca8336777bbd4fa1e3184f44b0ad7cb
Eli Ennab <elinaennab@gmail.com>
2022-11-22 11:42:25 +0100

    Added Shuffle an array, shuffle.js.

---
+++ lesson-8-and-9/shuffle.js
@@ -0,0 +1,8 @@
+/**
+ * Higher Order Array Methods
+ *
+ * Shuffle an array
+ */
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+console.log("Numbers before shuffling:", numbers);
