3bf0425aa0a982e162553d331f8901c70d0a7501
Eli Ennab <elinaennab@gmail.com>
2022-11-22 15:23:22 +0100

    Loop over the data + add the data in a HTML list.

--- lesson-xmlhttprequest/script.js
+++ lesson-xmlhttprequest/script.js
@@ -19,18 +19,28 @@ const request = new XMLHttpRequest();
 // Attach an event-listener to the request
 request.addEventListener('readystatechange', () => {
     // Is equest finished?
-    if (request.readyState === 4) {
+    if (request.readyState === 4) {     // 4 = DONE
         // console.log("Got me some data.", request);
 
         // Was the request successful?
         if (request.status === 200) {
+            // 200 OK
             console.log("200 OK!");
 
             // Take the STRING in responseText and PARSE it into Javascript object
             const data = JSON.parse(request.responseText);
 
             // Log the data
-            console.log("Data:", data);
+            // console.log("Got me some data:", data);
+            console.table(data);
+
+            // Loop over all users
+            data.forEach(user => {
+                // Append a listitem for each user
+                document.querySelector('#users').innerHTML += `<li>${user.name}</li>`;
+                // console.log(user.name);
+            });
+        
         } else {
             console.log("Something went wrong.");
         }
