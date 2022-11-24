517cbd850ad6f2bbe912473a30448d67b416296c
Eli Ennab <elinaennab@gmail.com>
2022-11-24 11:41:37 +0100

    Added lesson-pets including HTML, CSS, JS, JSON.

---
+++ lesson-pets/script.js
@@ -0,0 +1,38 @@
+/**
+ * Async pets
+ *
+ */
+
+ const getJSON = (url, callback) => {
+	// Create a new XML Http Request
+	const request = new XMLHttpRequest();
+
+	// Attach an event-listener to the request
+	request.addEventListener('readystatechange', () => {
+		// Check if readyState is 4 (=== DONE)
+		if (request.readyState === 4) {
+
+			// Was the request successful (200 = OK)?
+			if (request.status === 200) {
+				// Request was 200 OK!
+
+				// take the STRING in responseText and PARSE it into JavaScript object
+				const data = JSON.parse(request.responseText);
+
+				// invoke(call) callback
+				callback( undefined, data );
+
+			} else {
+				// Something went wrong :'(
+				console.log("ERROR ERROR DANGER WILL ROBINSON!");
+
+				// invoke(call) callback
+				callback( "Could not get data" );
+			}
+		}
+	});
+
+	request.open('GET', url);  // Set request to GET data
+	request.send();  // Send the request
+	console.log("Request sent!");
+}
