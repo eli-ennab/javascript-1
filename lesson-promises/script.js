3244d9a8ff9020c7f54180bcee2f517d83116267
Eli Ennab <elinaennab@gmail.com>
2022-11-24 13:28:45 +0100

    Promise with then and catch, teacher/exam example.

--- lesson-promises/script.js
+++ lesson-promises/script.js
@@ -1,4 +1,33 @@
 /**
- * Promises
+ * Promises 101
  *
  */
+
+const teacherIsNice = true;
+
+const willWeGetTheAnswersToTheExam = () => {
+	return new Promise ( (resolve, reject) => {
+		// do something that takes some time to do
+		// and when done, either resolve or reject the promise
+		setTimeout(() => {
+			if (teacherIsNice) {
+				resolve();
+			} else {
+			reject();
+			}
+		}, 2000);
+	} );
+};
+
+const teacherPromise = willWeGetTheAnswersToTheExam();
+// console.log(teacherPromise);
+
+teacherPromise
+	.then( () => {
+		console.log('YAY! We will get the exam.');
+	})
+	.catch( () => {
+		console.log('NEY! Teacher was not nice.');
+	});
+
+console.log('Promise started.');
