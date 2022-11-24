a8b5e62b44ba2e5f36336b567fc77c2151b6a365
Eli Ennab <elinaennab@gmail.com>
2022-11-24 14:07:51 +0100

    Add pets-code to later make it over to a promise.

--- lesson-promises/script.js
+++ lesson-promises/script.js
@@ -3,6 +3,74 @@
  * 
  */
 
+ const getJSON = (url, callback) => {
+	// Create a new XML Http Request
+	const request = new XMLHttpRequest();
+
+	// Attach an event-listener to the request
+	request.addEventListener('readystatechange', () => {
+		// Check if readyState is 4 (=== DONE)
+		if (request.readyState === 4) {
+
+			// Was the request successful (200 = OK)?
+			if (request.status === 200) {
+				// Request was 200 OK!
+
+				// take the STRING in responseText and PARSE it into JavaScript object
+				const data = JSON.parse(request.responseText);
+
+				// invoke(call) callback
+				callback( undefined, data );
+
+			} else {
+				// Something went wrong :'(
+				console.log("ERROR ERROR DANGER WILL ROBINSON!");
+
+				// invoke(call) callback
+				callback( "Could not get data" );
+			}
+		}
+	});
+
+	request.open('GET', url);  // Set request to GET data
+	request.send();  // Send the request
+	console.log("Request sent!");
+}
+
+// Get cats
+getJSON('data/cats.json', (err, cats) => {
+	if (err) {
+		alert("Could not get list of cats! Error was:" + err);
+		return;
+	}
+
+	// Got cats!
+	console.log("Got lists of cats:", cats);
+
+	// Get doggos
+	getJSON('data/dogs.json', (err, dogs) => {
+		// Got dogs!
+		console.log("Got lists of dogs:", dogs);
+
+		// Get birds
+		getJSON('data/birds.json', (err, birds) => {
+		// Got birds!
+		console.log("Got lists of birds:", birds);
+		});
+	});
+});
+
+// CALLBACK HELL
+getJSON('data/cats.json', (err, cats) => {
+	// Get doggos
+	getJSON('data/dogs.json', (err, dogs) => {
+		// Get birds
+		getJSON('data/birds.json', (err, birds) => {
+		})
+	})
+});
+
+/*
 const teacherIsNice = true;
 // const teacherIsNice = false;
 
@@ -33,3 +101,4 @@ willWeGetTheAnswersToTheExam()
 	});
 
 console.log('Promise started.');
+*/
