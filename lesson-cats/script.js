cdf8b3f9696fba18c955e61769e567f1d7cf89a6
Eli Ennab <elinaennab@gmail.com>
2022-11-25 14:58:20 +0100

    Cats shown!!!

--- lesson-cats/script.js
+++ lesson-cats/script.js
@@ -5,3 +5,21 @@
  * <https://cataas.com/cat?json=true>
  */
 
+const catImg = document.querySelector('#catImg');
+
+ const getCat = async () => {
+	const response = await fetch('https://cataas.com/cat?json=true');
+	if(!response.ok) {
+		throw new Error("Response was not OK.");
+	}
+	const cat = await response.json();
+
+	catImg.src = `https://cataas.com/${cat.url}`;
+};
+
+document.querySelector('#getCatBtn').addEventListener('click', () => {
+getCat();
+});
+
+// Start of with a
+getCat();
