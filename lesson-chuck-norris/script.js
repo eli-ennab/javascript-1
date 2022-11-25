cea45c1282934245cedd03e14091d19fff685744
Eli Ennab <elinaennab@gmail.com>
2022-11-25 13:31:05 +0100

    Fetch joke and output to DOM.

--- lesson-chuck-norris/script.js
+++ lesson-chuck-norris/script.js
@@ -5,6 +5,25 @@
  * <https://api.chucknorris.io/jokes/random>
  */
 
+// Promise with fetch
+fetch('https://api.chucknorris.io/jokes/random')
+	.then(response => {
+		if(!response.ok) {
+			throw new Error("Response was not OK.");
+		}
+
+		// output joke
+		return response.json();
+	})
+	.then(joke => {
+		// console.log("Joke:", joke);
+		document.querySelector('#joke').innerText = joke.value;
+	})
+	.catch(err => {
+		console.log("Could not fetch joke. Reason:", err);
+		document.querySelector('#joke').innerText = "ERROR. You don't summon Chuck Norris, Chuck Norris summons you.";
+	});
+
 
 
 
