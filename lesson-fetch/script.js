24f0c0e89764de13f24a1e55ca2efdaa9fcbbf38
Eli Ennab <elinaennab@gmail.com>
2022-11-25 11:20:40 +0100

    Fetching data of cats and dogs.

--- lesson-fetch/script.js
+++ lesson-fetch/script.js
@@ -2,3 +2,37 @@
  * Fetch
  * 
  */
+
+fetch('data/cats.json')
+	.then(response => {
+		console.log("Response:", response);
+		// check if response was ok
+		if (!response.ok) {
+			throw new Error("Response was not OK!");
+		}
+		return response.json()
+	})
+	.then(data => {
+		console.log("Gots data:", data);
+	})
+	.catch(err => {
+		console.log("Something went wrong:", err);
+	});
+
+fetch('data/dogs.json')
+	.then(response => {
+		if (!response.ok) {
+			throw new Error("Response was not OK!");
+		}
+		return response.json()
+	})
+	.then(data => {
+		console.log("Gots data:", data);
+	})
+	.catch(err => {
+		console.log("Something went wrong:", err);
+	});
+
+
+
+
