bbc8b4eb391c59f149ba51e17689812d6a6458b2
Eli Ennab <elinaennab@gmail.com>
2022-11-25 13:41:21 +0100

    Jokes with fetch and get joke button.

--- inlamningsuppgift-tester/shuffle.js
+++ inlamningsuppgift-tester/shuffle.js
@@ -15,3 +15,36 @@ const names = ["Abdirahman Ali", "Albin Lindeborg", "Alexander Olsson", "Amer El
 
 shuffleArray(names);
 console.log("Shuffled student:", names[0]);
+
+
+
+
+
+
+
+
+// Click event
+// Promise with fetch
+// const getJoke = () => {
+// 	document.querySelector('#getJokeBtn').addEventListener('click', () => {
+// 		fetch('https://api.chucknorris.io/jokes/random')
+// 		.then(response => {
+// 			if(!response.ok) {
+// 				throw new Error("Response was not OK.");
+// 			}
+
+// 			// output joke
+// 			return response.json();
+// 		})
+// 		.then(joke => {
+// 			// console.log("Joke:", joke);
+// 			document.querySelector('#joke').innerText = joke.value;
+// 		})
+// 		.catch(err => {
+// 			console.log("Could not fetch joke. Reason:", err);
+// 			document.querySelector('#joke').innerText = "ERROR. You don't summon Chuck Norris, Chuck Norris summons you.";
+// 		});
+// 	})
+// };
+
+// getJoke();
--- lesson-chuck-norris/script.js
+++ lesson-chuck-norris/script.js
@@ -6,24 +6,29 @@
  */
 
 // Promise with fetch
-fetch('https://api.chucknorris.io/jokes/random')
-	.then(response => {
-		if(!response.ok) {
-			throw new Error("Response was not OK.");
-		}
+const getJoke = () => {
+		fetch('https://api.chucknorris.io/jokes/random')
+		.then(response => {
+			if(!response.ok) {
+				throw new Error("Response was not OK.");
+			}
 
-		// output joke
-		return response.json();
-	})
-	.then(joke => {
-		// console.log("Joke:", joke);
-		document.querySelector('#joke').innerText = joke.value;
-	})
-	.catch(err => {
-		console.log("Could not fetch joke. Reason:", err);
-		document.querySelector('#joke').innerText = "ERROR. You don't summon Chuck Norris, Chuck Norris summons you.";
-	});
+			// output joke
+			return response.json();
+		})
+		.then(joke => {
+			// console.log("Joke:", joke);
+			document.querySelector('#joke').innerText = joke.value;
+		})
+		.catch(err => {
+			console.log("Could not fetch joke. Reason:", err);
+			document.querySelector('#joke').innerText = "ERROR. You don't summon Chuck Norris, Chuck Norris summons you.";
+		});
+};
 
+document.querySelector('#getJokeBtn').addEventListener('click', () => {
+	getJoke();
+});
 
-
+getJoke();
 
