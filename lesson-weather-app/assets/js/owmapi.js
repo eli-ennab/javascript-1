c1caf71529617dcbddfbaa088c510a41b78c3eb4
Eli Ennab <elinaennab@gmail.com>
2022-11-29 14:45:29 +0100

    Added instructions to workshop and fake slow API.

--- lesson-weather-app/assets/js/owmapi.js
+++ lesson-weather-app/assets/js/owmapi.js
@@ -7,6 +7,9 @@
 const API_KEY = "27ba265b00ae8fb3973222c07a9d6f40";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
+const FAKE_SLOW_API = true;
+const FAKE_SLOW_API_DELAY = 3000;
+
 const getCurrentWeather = async (city) => {
     // Get weather for city from OpenWeatherMap API
     const response = await fetch(`${BASE_URL}/weather?q=${city}&units=metric&appid=${API_KEY}`);
@@ -18,6 +21,9 @@ const getCurrentWeather = async (city) => {
 
     // Convert response from JSON
     const data = await response.json();
+
+    // fake a slow api
+    FAKE_SLOW_API && await new Promise(r => setTimeout(r, FAKE_SLOW_API_DELAY));
 
     // Return current weather
     return data;
