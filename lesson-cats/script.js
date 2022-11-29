0f021735e7bb5800c3f5871211fa23e2de52143d
Eli Ennab <elinaennab@gmail.com>
2022-11-29 11:00:56 +0100

    try ... catch

--- lesson-cats/script.js
+++ lesson-cats/script.js
@@ -33,16 +33,27 @@ const baseURL = 'https://cataas.com';
 const getJSON = async(url) => {
 	const response = await fetch(url);				// we want to wait (await) for the promise to be resolved (fetch)
 	if (!response.ok) {
-		throw new Error(`Response was not OK. Status returned was "${res.status} ${res.statusText}".`);
+		throw new Error(`Response was not OK. Status returned was "${response.status} ${response.statusText}".`); // throws the error
 	}
 	return await response.json();					// returns a promise (the result of the promise when resolved)
 }
 
 const getNewCat = async () => {
-	console.log('New cat.');
+	const errorEl = document.querySelector('#error');
+	errorEl.classList.add('hide');
 
-	const cat = await getJSON(`${baseURL}/cat?json=true`)
-	document.querySelector('#catImg').src = baseURL + cat.url;
+	try {
+		const cat = await getJSON(`${baseURL}/cat?json=true`);
+		document.querySelector('#catImg').src = baseURL + cat.url;
+	} catch (e) {									// e is error/exception and catches all the errors happening in the block
+		console.log('Caught the error', e);
+
+		// Show error message for user
+		errorEl.innerText = e;
+		errorEl.classList.remove('hide');
+	} finally {
+		// Do this either if success or fail
+	}
 }
 
 document.querySelector('#getCatBtn').addEventListener('click', () => {
