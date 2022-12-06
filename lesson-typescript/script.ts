b8a796fd7b3480c7d529e12850ae3597e5846ecd
Eli Ennab <elinaennab@gmail.com>
2022-12-06 10:52:59 +0100

    Update JS-file automatically with tsc -w script.ts

--- lesson-typescript/script.ts
+++ lesson-typescript/script.ts
@@ -4,7 +4,9 @@
  * Watch this file for changes by running
  * the following command in the terminal
  * tsc -w script.ts
- *
+ * 
  */
+
 console.log("Hello TypeScript");
+
 console.log("Hello again, TypeScript");
