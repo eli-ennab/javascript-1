0d39a1077b2232faeee24d5d67e4406c27e8dade
Eli Ennab <elinaennab@gmail.com>
2022-12-07 14:10:26 +0100

    Video 10, function signatures.

--- typescript-tutorial/sandbox.ts
+++ typescript-tutorial/sandbox.ts
@@ -199,3 +199,34 @@ const greetAgain = (user: objWithName) => {
     console.log(`${user.name} says hello again`)
 }
 */
+
+/*
+* Video #10 Function Signatures
+*/
+
+// let greet: Function
+
+// Example 1
+let greet: (a: string, b: string) => void               // return void
+greet = (name:string, greeting: string) => {
+    console.log(`${name} says ${greeting}`)
+}
+
+// Example 2
+let calc: (a: number, b: number, c: string) => number   // return number
+calc = (numOne: number, numTwo: number, action: string) => {
+    if (action === 'add') {
+        return numOne + numTwo;
+    } else {
+        return numOne - numTwo
+    }
+}
+
+// Example 3
+let logDetails: (obj: {name: string, age: number}) => void
+
+type person = {name: string, age: number}
+
+logDetails = (ninja: person) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`)
+}
