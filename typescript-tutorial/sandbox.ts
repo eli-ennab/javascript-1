ef195fa0d35414da0eb00db86caba7f767469dce
Eli Ennab <elinaennab@gmail.com>
2022-12-07 14:56:54 +0100

    Video 13, public, private and readonly.

--- typescript-tutorial/sandbox.ts
+++ typescript-tutorial/sandbox.ts
@@ -273,6 +273,7 @@ form.addEventListener('submit', (e: Event) => {
 * Video #12 Classes
 */
 
+/*
 class Invoice {
     client: string
     details: string
@@ -301,3 +302,31 @@ class Invoice {
   
   console.log(invOne, invTwo)
   console.log(invoices)
+  */
+
+  /*
+* Video #13 Public, private and readonly
+*/
+
+class Invoice {
+    constructor(
+      readonly client: string,
+      private details: string,
+      public amount: number
+    ){}
+  
+    format() {
+      return `${this.client} owes £${this.amount} for ${this.details}`
+    }
+  }
+  
+  const invOne = new Invoice('mario', 'work on the mario website', 250)
+  const invTwo = new Invoice('luigi', 'work on the luigi website', 300)
+  
+  let invoices: Invoice[] = []
+  invoices.push(invOne)
+  invoices.push(invTwo)
+  
+  invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format())
+  })
