dad3ccab992f091ec76442ebd3bfcb4f0e7dd9f0
Eli Ennab <elinaennab@gmail.com>
2022-12-08 09:23:23 +0100

    Added lesson typescript vite.

---
+++ lesson-typescript-vite/src/main.ts
@@ -0,0 +1,21 @@
+import './style.css'
+
+const todosList = document.querySelector('#todos')
+const newTodoForm = document.querySelector('#new-todo-form')
+
+const getRandomNumber = (max: number = 10) => {
+	return Math.ceil( Math.random() * max )
+}
+const num = getRandomNumber(40)
+console.log("Random number:", num)
+
+newTodoForm?.addEventListener('submit', e => {
+	e.preventDefault()
+
+	const inputNewTodoTitle = document.querySelector<HTMLInputElement>('#new-todo-title')!
+	const newTodoTitle = inputNewTodoTitle.value
+
+	if (todosList) {
+		todosList.innerHTML += `<li>${newTodoTitle}</li>`
+	}
+})
