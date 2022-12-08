f3971a235a09f09b09459da0b86c5fd47b9f63aa
Eli Ennab <elinaennab@gmail.com>
2022-12-08 10:29:17 +0100

    Render todos and into the DOM.

--- 39-typescript-vite/src/main.ts
+++ 39-typescript-vite/src/main.ts
@@ -1,16 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import './style.css'
 
-const todosList = document.querySelector('#todos')
+const todosList = document.querySelector('#todos')!
 const newTodoForm = document.querySelector('#new-todo-form')
 
+// list of todos
+const todos: string[] = []
+// console.log(todos)
+
+// render todos
+const renderTodos = () => {
+	todosList.innerHTML = ''
+
+	todos.forEach(todo => {
+		todosList.innerHTML += `<li class="list-group-item">${todo}</li>`
+	})
+}
+
+// Create a new todo form
 newTodoForm?.addEventListener('submit', e => {
 	e.preventDefault()
 
 	const inputNewTodoTitle = document.querySelector<HTMLInputElement>('#new-todo-title')!
 	const newTodoTitle = inputNewTodoTitle.value
 
-	if (todosList) {
-		todosList.innerHTML += `<li>${newTodoTitle}</li>`
-	}
+	// push todo into list of todos
+	todos.push(newTodoTitle)
+	// console.log(todos)
+
+	// render all todos
+	renderTodos()
 })
+
+// render all todos
+renderTodos()
