1438902b59f2967d3b380decd8e12681a354db95
Eli Ennab <elinaennab@gmail.com>
2022-12-15 13:11:30 +0100

    Added a function to update todos, not working tho.

--- 40-async-todos-ts/frontend/src/main.ts
+++ 40-async-todos-ts/frontend/src/main.ts
@@ -109,57 +109,59 @@ getTodos()
 * Tries with FETCH on clicked and updated todo
 */
 
-// function updateTodo(): ITodo {
-//     const completedTodo: ITodo = {
-//         ...todos,
-//         completed: true
-//     }
+// const updateTodo = async (todoId: ITodo, data: ITodo) => {
+// 	const res = await fetch(`http://localhost:3001/todos/${todoId}`, {
+// 		method: 'PATCH',
+// 		headers: {
+// 			'Content-Type': 'application/json',
+// 		},
+// 		body: JSON.stringify(data),
+// 	});
 
-//     return completedTodo
+// 	// Check that everything went ok
+// 	if (!res.ok) {
+// 		throw new Error(`Could not update todo, reason: ${res.status} ${res.statusText}`);
+// 	}
+
+// 	return await res.json();
 // }
 
-// function updateClickedTodo(): ITodo {
-    // Listen for not completed todos and change to completed todo
-    document.querySelector('#todos')?.addEventListener('click', async (e) => {
-        const target = e.target as HTMLElement
-        let todos = document.getElementById("todos") as HTMLDivElement;
-        let completedTodos = document.getElementById("completed-todos") as HTMLDivElement;
-        let id = target.dataset.todoId
-        let completed = target.dataset.todoCompleted
+// Listen for not completed todos and change to completed todo
+document.querySelector('#todos')?.addEventListener('click', async (e) => {
+    const target = e.target as HTMLElement
+    let todos = document.getElementById("todos") as HTMLDivElement;
+    let completedTodos = document.getElementById("completed-todos") as HTMLDivElement;
+    let clickedTodoId = target.dataset.todoId
+    let completedStatus = target.dataset.todoCompleted
 
-        if (target.tagName === "LI") {
-            // Finding the todo's ID
-            console.log(`This is the todo's ID:`, id)
-            console.log(`This is the completed status:`, completed)
+    if (target.tagName === "LI") {
+        // Finding the todo's ID
+        console.log(`This is a todo with ID:`, clickedTodoId, `with completed status:`, completedStatus)
 
-            // Remove the list item from its current parent div
-            todos.removeChild(target);
+        // Remove the list item from its current parent div
+        todos.removeChild(target);
 
-            // Add the list item to the new div
-            completedTodos.appendChild(target);
-        } 
-    });
+        // Add the list item to the new div
+        completedTodos.appendChild(target);
+    } 
+});
 
-    // Listen for completed todos and change to not completed todo
-    document.querySelector('#completed-todos')?.addEventListener('click', async (e) => {
-        const target = e.target as HTMLElement
-        let todos = document.getElementById("todos") as HTMLDivElement;
-        let completedTodos = document.getElementById("completed-todos") as HTMLDivElement;
-        let id = target.dataset.todoId
-        let completed = target.dataset.todoCompleted
+// Listen for completed todos and change to not completed todo
+document.querySelector('#completed-todos')?.addEventListener('click', async (e) => {
+    const target = e.target as HTMLElement
+    let todos = document.getElementById("todos") as HTMLDivElement;
+    let completedTodos = document.getElementById("completed-todos") as HTMLDivElement;
+    let clickedTodoId = target.dataset.todoId
+    let completedStatus = target.dataset.todoCompleted
 
-        if (target.tagName === "LI") {
-            // Finding the todo's ID
-            console.log(`This is the todo's ID:`, id)
-            console.log(`This is the completed status:`, completed)
+    if (target.tagName === "LI") {
+        // Finding the todo's ID
+        console.log(`This is a todo with ID:`, clickedTodoId, `with completed status:`, completedStatus)
             
-            // Remove the list item from its current parent div
-            completedTodos.removeChild(target);
+        // Remove the list item from its current parent div
+        completedTodos.removeChild(target);
 
-            // Add the list item to the new div
-            todos.appendChild(target);
-        } 
-    });
-// }
-
-// updateClickedTodo():
+        // Add the list item to the new div
+        todos.appendChild(target);
+    } 
+});
